bfa59b9bd2e19115490c14e68cf03b76b5a6258f
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-09-10 14:08:47 +0430

    add UserManager by BaseUserManager

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,18 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
-class User(AbstractBaseUser , PermissionsMixin):
-    email = models.EmailField(max_length = 255 , unique=True)
-    is_staff = models.BooleanField(default = False)
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password and extra date.
+        """
+        if not email:
+            raise ValueError(_("the Email must be set."))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password and extra date.
+        """
+        extra_fields.setdefault('in_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User for our app.
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     # is_verified = models.CharField(default = False)
     first_name = models.CharField(max_length=20)
     REQUIRED_FIELDS = []
 
-    created_date = models.DateTimeField(auto_now_add = True)
-    updated_date = models.DateTimeField(auto_now = True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    objects = MyUserManager()
 
     def __str__(self):
         return self.email
