1a96dca7bea667c444f4184c74f6f53426b4b59e
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-09-11 08:20:12 +0430

    add signal to model(for connect User to Profile)

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, PermissionsMixin)
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -65,4 +67,9 @@ class Profile(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        self.user.email
+       return self.user.email
+
+@receiver(post_save,sender=User)
+def save_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
