332576c63030d40d5dbfe217ea4a4141d325c527
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-09-12 22:35:32 +0430

    first cbv I have tried

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -3,5 +3,6 @@ from blog import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('fbv-index', views.indexView, name='fbv-index')
+    path('fbv-index', views.indexView, name='fbv-index'),
+    path('cbv-index',views.IndexView.as_view() , name='cbv-index')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -9,3 +9,12 @@ def indexView(request):
     name = 'ali'
     context = {'name': name}
     return render(request, 'website/index.html', context)
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'ali'
+        context['posts'] = Post.objects.all()
+        return context
