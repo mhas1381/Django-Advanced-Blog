1d9694d550a1330e160710866ce71b7a9eb0844b
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-09-23 09:52:52 +0330

    use ListView to show posts

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,redirect
-from django.views.generic.base import TemplateView,RedirectView
+from django.shortcuts import render, redirect
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView
 from blog.models import *
 
 # Create your views here.
@@ -11,22 +12,38 @@ def indexView(request):
     context = {'name': name}
     return render(request, 'website/index.html', context)
 '''
+
+
 class IndexView(TemplateView):
     '''
     a class based view to show index page
     '''
     template_name = 'index.html'
 
-    def get_context_data(self,**kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['name'] = 'ali'
         context['posts'] = Post.objects.all()
         return context
 
 # FBV for redirect
+
+
 def redirectToMaktab(request):
     return redirect('https://maktabkhooneh.org')
 
 # CBV for redirect
+
+
 class RedirectToMaktab(RedirectView):
     url = 'https://maktabkhooneh.org'
+
+
+class PostList(ListView):
+    # model = Post
+    # queryset = Post.objects.all()
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        posts = Post.objects.filter(status=True)
+        return posts
