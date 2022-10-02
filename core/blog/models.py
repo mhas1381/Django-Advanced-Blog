eb1af2790cc050c670352f32e3b27ace40fb861d
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-10-02 15:26:30 +0330

    migrated from user to profile

--- core/blog/models.py
+++ core/blog/models.py
@@ -2,26 +2,29 @@ from django.db import models
 from django.contrib.auth import get_user_model
 # Create your models here.
 
-#getting user model object
-User = get_user_model()
+# getting user model object
+# User = get_user_model()
+
 
 class Post(models.Model):
     '''
     this is a class for define posts for blog app
     '''
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
-    image = models.ImageField(upload_to ='blog/',null = True , blank =True)
+    author = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog/', null=True, blank=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     status = models.BooleanField()
-    category = models.ForeignKey('Category', on_delete = models.SET_NULL,null = True)
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True)
 
     created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now = True)
-    published_date = models.DateTimeField(null = True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.title
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
