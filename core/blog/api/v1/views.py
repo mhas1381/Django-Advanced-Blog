24f9b423fee0a69c8edc5a5df9774711b6e9fe56
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-01-28 15:39:38 +0330

    return 404 method 2(get object or 404)

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from ...models import Post
 
 
@@ -12,9 +13,7 @@ def post_list(request):
 
 @api_view()
 def post_detail(request, id):
-    try:
-        post = Post.objects.get(pk=id)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-    except Post.DoesNotExist:
-        return Response({"detail": "Post does not exist"} , status = status.HTTP_404_NOT_FOUND)
+
+    post = get_object_or_404(Post , pk=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
