cc906721bf1a2e64207bf8d11269fad4997d8ed2
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-01-28 15:54:24 +0330

    use modelSerializer to get post information

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from blog.models import Post
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length = 255)
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField(  )
-    title = serializers.CharField(max_length = 255)
-    
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'status', 'published_date', ]
