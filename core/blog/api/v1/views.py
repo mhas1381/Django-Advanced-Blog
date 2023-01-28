5bbd1519ee4dee0dcd6f2fe6745a462570c9a508
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-01-28 15:37:51 +0330

    return 404 from rest method 1

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,14 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
+from rest_framework import status
 from ...models import Post
+
 
 @api_view()
 def post_list(request):
-    return Response({"name":"Mohammad Hussein"})
+    return Response({"name": "Mohammad Hussein"})
+
 
 @api_view()
-def post_detail(request,id):
-    post = Post.objects.get(pk=id)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+def post_detail(request, id):
+    try:
+        post = Post.objects.get(pk=id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    except Post.DoesNotExist:
+        return Response({"detail": "Post does not exist"} , status = status.HTTP_404_NOT_FOUND)
