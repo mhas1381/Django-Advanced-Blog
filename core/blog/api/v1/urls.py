5d2638399aff19d13fe5a8707298523da4d94a96
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-01-28 15:27:05 +0330

    create post serializer

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -6,5 +6,6 @@ app_name = 'api-v1'
 
 urlpatterns = [
     path('post/',views.post_list,name='post-list'),
+    path('post/<int:id>/',views.post_detail,name='post-detail'),
 
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from ...models import Post
 
 @api_view()
 def post_list(request):
     return Response({"name":"Mohammad Hussein"})
+
+@api_view()
+def post_detail(request,id):
+    post = Post.objects.get(pk=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
