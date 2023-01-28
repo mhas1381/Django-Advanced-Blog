6bfce89c20336ba0f4a1c72b8b95a5c97300d3e8
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-01-29 00:51:33 +0330

    api permissions

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .serializers import PostSerializer
 from rest_framework import status
 
@@ -8,6 +9,7 @@ from ...models import Post
 
 
 @api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
 def post_list(request):
     if request.method == "GET":
         posts = Post.objects.filter(status=1)
