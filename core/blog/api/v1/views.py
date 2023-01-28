1bcfbb0fc464050ea388a8c49257df288e383d40
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-01-28 23:17:18 +0330

    updating post with api

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from rest_framework import status
-from django.shortcuts import get_object_or_404
+
 from ...models import Post
 
 
@@ -13,15 +14,20 @@ def post_list(request):
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
     elif request.method == "POST":
-        serializer = PostSerializer(data = request.data)
-        serializer.is_valid(raise_exception = True)
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def post_detail(request, id):
-
     post = get_object_or_404(Post, pk=id)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+    if request.method == "GET":
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PostSerializer(post , data = request.data)
+        serializer.is_valid(raise_exception)
+        serializer.save()
+        return Response(serializer.data)
