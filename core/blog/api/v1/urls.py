bf2b09309eca97bb634d746d50e9cc3b27d3f97f
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-01-30 00:18:47 +0330

    use CBV for post list function api

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('post/',views.post_list,name='post-list'),
+    # path('post/',views.post_list,name='post-list'),
+    path('post/',views.PostList.as_view(),name='post-list'),
     path('post/<int:id>/',views.post_detail,name='post-detail'),
 
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import PostSerializer
+from rest_framework.views import APIView
 from rest_framework import status
+from .serializers import PostSerializer
 
 from ...models import Post
 
 
-@api_view(["GET", "POST"])
+'''@api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
 def post_list(request):
     if request.method == "GET":
@@ -16,6 +17,23 @@ def post_list(request):
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
     elif request.method == "POST":
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)'''
+
+class PostList(APIView):
+    '''getting list of posts and creating post new posts'''
+
+    def get(self,request):
+        '''retreive a list of posts'''
+        if request.method == "GET":
+            posts = Post.objects.filter(status=1)
+            serializer = PostSerializer(posts, many=True)
+            return Response(serializer.data)
+    
+    def post(self,request):
+        '''creating a new post with provided data'''
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
