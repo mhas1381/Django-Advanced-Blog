0e882f718e0033a2c452c9d5910bd233dc728197
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-05-17 11:27:13 +0330

    add ordering filter

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 from .serializers import PostSerializer,CategorySerializer
 from ...models import Post,Category
 from .permissions import IsOwnerOrReadOnly
@@ -17,9 +17,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
     filterset_fields = ['category', 'author']
     search_fields = ['=title', 'content']
+    ordering_fields = ['published_date']
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     ''' getting detail of the categories ,edit and removing it'''
