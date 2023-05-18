7d57e15a2237bd87210e68f2af8306acdec00583
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-05-18 10:49:47 +0330

    add swagger and redoc

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -36,7 +36,7 @@ class PostSerializer(serializers.ModelSerializer):
             rep.pop('relative_url', None)
             rep.pop('absolute_url', None)
         else:
-            rep.pop('context', None)
+            rep.pop('content', None)
         rep['category'] = CategorySerializer(instance.category , context = {'request':request}).data
         return rep
     
--- core/core/settings.py
+++ core/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'blog',
     'rest_framework',
     'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
