c778fbd9b53b460831b39348a29db9974a4f006c
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-05-18 11:04:32 +0330

    get api json format

--- core/core/urls.py
+++ core/core/urls.py
@@ -25,12 +25,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="API for my projuct",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      contact=openapi.Contact(email="mhas.software@gmail.com"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
@@ -43,7 +43,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('api-docs/' , include_docs_urls(title = 'api sample')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/api.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
