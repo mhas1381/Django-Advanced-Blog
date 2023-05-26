f19ffe00a9f1ab55bff149280902f690f6f7137e
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-05-26 22:44:44 +0330

    login token

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import ObtainAuthToken
 from . import views
 app_name='api-v1'
 
 urlpatterns = [
     #registration
-    path('registration/' ,views.RegistrationApiView.as_view() , name = 'registration' )
+    path('registration/' ,views.RegistrationApiView.as_view() , name = 'registration' ),
+    path('token/login' , ObtainAuthToken.as_view(),name='token-login')
     #change password
     #reset password
     #login token
