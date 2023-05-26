b212afd9fe3f2132713ee3af4486df92df634f3e
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-05-26 19:38:16 +0330

    start api for accounting

--- core/core/urls.py
+++ core/core/urls.py
@@ -39,8 +39,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
+
     path('api-docs/' , include_docs_urls(title = 'api sample')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger/api.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
