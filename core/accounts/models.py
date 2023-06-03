5cfcb414d2ebd4d10ad51e7cdf719d526c2b176d
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-06-03 23:26:27 +0330

    remove is verified

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -43,7 +43,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_verified = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
