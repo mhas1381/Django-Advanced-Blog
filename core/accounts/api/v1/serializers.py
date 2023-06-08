6be25cc956f1ae61c42174f04f7078b991615425
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-06-08 11:49:49 +0330

    add is_verifies

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -56,8 +56,8 @@ class CustomAuthTokenSerializer(serializers.Serializer):
             if not user:
                 msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg, code="authorization")
-            # if not user.is_verified:
-            #     raise serializers.ValidationError({"details": "user is not verified"})
+            if not user.is_verified:
+                raise serializers.ValidationError({"details": "user is not verified"})
         else:
             msg = _('Must include "username" and "password".')
             raise serializers.ValidationError(msg, code="authorization")
@@ -68,8 +68,8 @@ class CustomAuthTokenSerializer(serializers.Serializer):
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         validated_data = super().validate(attrs)
-        # if not self.user.is_verified:
-        #     raise serializers.ValidationError({"details": "user is not verified"})
+        if not self.user.is_verified:
+            raise serializers.ValidationError({"details": "user is not verified"})
         validated_data["email"] = self.user.email
         validated_data["user_id"] = self.user.id
         return validated_data
