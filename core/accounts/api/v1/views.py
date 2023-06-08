0008ec6680c7b51013f8573fa22b5b6883ec8d87
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-06-08 18:35:52 +0330

    Creating tokens manually

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -11,6 +11,8 @@ from mail_templated import send_mail
 from .serializers import (RegistrationSerializer,CustomAuthTokenSerializer,
         CustomTokenObtainPairSerializer,ChangePasswordSerialier,ProfileSerializer)
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.shortcuts import get_object_or_404
 from mail_templated import EmailMessage
 from ..utils import EmailThread
 from ...models import Profile
@@ -91,9 +93,13 @@ class ProfileApiView(generics.RetrieveUpdateAPIView):
 class TestEmailSend(generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
-
-
-        email_obj = EmailMessage('email/hello.tpl', {'name': 'ali'}, 'admin@admin.com',to=['mhas.software@gmail.com'])
+        self.email = "mhas.software@gmail.com"
+        user_obj = get_object_or_404(User , email = self.email)
+        token = self.get_tokens_for_user(user_obj)
+        email_obj = EmailMessage('email/hello.tpl', {'token': token }, 'admin@admin.com',to=[self.email])
         EmailThread(email_obj).start()
-
         return Response('email sent')
+
+    def get_tokens_for_user(self , user):
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
--- core/templates/email/hello.tpl
+++ core/templates/email/hello.tpl
@@ -1,9 +1,9 @@
 {% extends "mail_templated/base.tpl" %}
 
 {% block subject %}
-Hello 
+Activation
 {% endblock %}
 
 {% block html %}
-This is an <strong>html</strong> message.
+{{token}}
 {% endblock %}
