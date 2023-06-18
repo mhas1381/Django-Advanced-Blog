137f14252ec9d4eb0fe73f92f605bab109fce8bb
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-06-18 08:09:37 +0330

    first test case for blog api

---
+++ core/blog/tests/test_post_api.py
@@ -0,0 +1,11 @@
+from rest_framework.test import APIClient
+from django.urls import reverse
+import pytest
+
+@pytest.mark.django_db
+class TestPostApi:
+    def test_get_post_response_200_status(self):
+        client = APIClient()
+        url = reverse("blog:api-v1:post-list")
+        response = client.get(url)
+        assert response.status_code ==200
