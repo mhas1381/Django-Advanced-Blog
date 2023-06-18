bc4d9e16ae888dbfa37c6ab1c32903658b91a3c7
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-06-18 08:18:04 +0330

    test case for create post

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,6 +1,7 @@
 from rest_framework.test import APIClient
 from django.urls import reverse
 import pytest
+from datetime import datetime
 
 @pytest.mark.django_db
 class TestPostApi:
@@ -9,3 +10,14 @@ class TestPostApi:
         url = reverse("blog:api-v1:post-list")
         response = client.get(url)
         assert response.status_code ==200
+
+    def test_create_post_response_401_status(self, api_client):
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            "title": "test",
+            "content": "description",
+            "status": True,
+            "published_date": datetime.now(),
+        }
+        response = api_client.post(url, data)
+        assert response.status_code == 401
